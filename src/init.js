a0a78d5931c815a06619aa8bc62c7ec68427b22e
MKS-students <mks.students@hackreactor.com>
2015-07-07 20:58:19 -0700

    Complete dance party.

--- src/init.js
+++ src/init.js
@@ -2,32 +2,40 @@ $(document).ready(function(){
   window.dancers = [];
 
   $(".addDancerButton").on("click", function(event){
-    /* This function sets up the click handlers for the create-dancer
-     * buttons on dancefloor.html. You should only need to make one small change to it.
-     * As long as the "data-dancer-maker-function-name" attribute of a
-     * class="addDancerButton" DOM node matches one of the names of the
-     * maker functions available in the global scope, clicking that node
-     * will call the function to make the dancer.
-     */
-
-    /* dancerMakerFunctionName is a string which must match
-     * one of the dancer maker functions available in global scope.
-     * A new object of the given type will be created and added
-     * to the stage.
-     */
     var dancerMakerFunctionName = $(this).data("dancer-maker-function-name");
-
-    // get the maker function for the kind of dancer we're supposed to make
     var dancerMakerFunction = window[dancerMakerFunctionName];
-
-    // make a dancer with a random position
-
-    var dancer = dancerMakerFunction(
+    var dancer = new dancerMakerFunction(
       $("body").height() * Math.random(),
       $("body").width() * Math.random(),
       Math.random() * 1000
     );
     $('body').append(dancer.$node);
+    window.dancers.push(dancer.$node);
+  });
+
+  $(".lineUpButton").on("click", function(event) {
+    $('.dancer').addClass('lineUp');
+  });
+
+  $("body").on("mousemove", ".mario-mushroom", function(event) {
+    $(this).css({'left' : event.pageX, 'top' : event.pageY}).on('click', function(event) {
+      $(this).hide( "explode", {pieces:64}, 1000);
+    });
+  });
+
+  $("body").on("click", ".mario-mushroom2", function(event) {
+    $(this).hide( "explode", {pieces:64}, 1000);
+  });
+
+  $("body").on("click", ".explodeButton", function(event) {
+    $(this).hide( "explode", {pieces:64}, 1000);
+  });
+
+  $( "body" ).on("mouseover", ".mario-mushroom3", function(event) {
+      $( ".mario-mushroom3" ).animate({
+        border: "20px solid cyan",
+        width: 20,
+        height:20
+      }, 0);
   });
 });
-
